904ad3cdc11fcd66a90bc61f98b8a468fc74a0f2
AbbleCorp <a.gitgud@theomers.com>
2025-10-03 14:53:12 -0600

    add configurations for testing

--- tests/pizza.spec.ts
+++ tests/pizza.spec.ts
@@ -1,8 +1,21 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from 'playwright-test-coverage';
 
-test('has title', async ({ page }) => {
+test('home page loads', async ({ page }) => {
   await page.goto('http://localhost:5173/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle('JWT Pizza');
+});
+
+
+test('login', async ({ page }) => {
+  await page.goto('http://localhost:5173/');
+
+  await page.getByRole('link', { name: 'Login' }).click();
+  await page.getByRole('textbox', { name: 'Email address' }).click();
+  await page.getByRole('textbox', { name: 'Email address' }).fill('a@jwt.com');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('admin');
+
+  await page.getByRole('button', { name: 'Login' }).click();
 });
